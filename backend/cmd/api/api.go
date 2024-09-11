cc5611da2c1670df2be7707212ef8891a5486a0f
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-09-11 22:54:14 +0700

    first commit

---
+++ backend/cmd/api/api.go
@@ -0,0 +1,15 @@
+package api
+
+import "database/sql"
+
+type APIServer struct {
+	address string
+	db      *sql.DB
+}
+
+func NewAPIServer(address string, db *sql.DB) *APIServer {
+	return &APIServer{
+		address: address,
+		db:      db,
+	}
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module thitiphongD/ecom-echo
+
+go 1.22.1
